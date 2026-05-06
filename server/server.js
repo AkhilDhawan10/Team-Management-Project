705517986786f2e646a86819a218f3dc8e8b4971
akhil-successiveTech <akhil.dhawan@successive.tech>
2026-05-06 15:52:01 +0530

    Updated API URL for Railway deployment and enabled CORS for all origins

--- server/server.js
+++ server/server.js
@@ -11,7 +11,9 @@ const taskRoutes = require('./routes/tasks');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 app.use(express.json());
 
 // API Routes
@@ -33,7 +35,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Internal server error' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
