6502f58df2355a8b5b335f5f58d877b509df0cb2
akhil-successiveTech <akhil.dhawan@successive.tech>
2026-05-06 16:08:32 +0530

    fixed railway vite host issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 3000,
     proxy: {
@@ -12,6 +13,12 @@ export default defineConfig({
       },
     },
   },
+
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: true,
+  },
+
   build: {
     outDir: 'dist',
   },
